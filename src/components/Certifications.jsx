9ba14fdef5a5ea43f6dbc63dd3a26be109e216b2
Malipolishivani330 <malipolishivani3027@gmail.com>
2026-06-20 20:22:10 +0530

    Fixed portfolio deployment

--- src/components/Certifications.jsx
+++ src/components/Certifications.jsx
@@ -5,7 +5,8 @@ function Certifications() {
   const certifications = [
 
     {
-      title: "Python Full Stack Development - PR Softwares Training Institute",
+      title: "Python Full Stack Development",
+      issuer: "PR Softwares Training Institute",
       year: "2025",
       icon: "🐍",
       link: "https://drive.google.com/file/d/1gQqEy9T_nD23VC1uCxtzb9wCj8TLSh-H/view?usp=sharing"
@@ -13,13 +14,15 @@ function Certifications() {
 
     {
       title: "AWS Cloud Practitioner Essentials",
+      issuer: "AWS",
       year: "2026",
       icon: "☁️",
       link: "https://drive.google.com/file/d/1EKs3B0YLSY8wUwfPxmCXUHDidAfvWk2_/view?usp=sharing"
     },
 
     {
-      title: "Amazon ECS - KodeKloud",
+      title: "Amazon ECS",
+      issuer: "Kodekloud",
       year: "2026",
       icon: "🚀",
       link: "https://learn.kodekloud.com/certificate/aebf0106-a7c8-4795-8b5f-0f62cca34642"
@@ -27,7 +30,8 @@ function Certifications() {
 
     
     {
-      title: "AI Fundamentals - IBM SkillsBuild",
+      title: "AI Fundamentals",
+      issuer: "IBM SkillsBuild",
       year: "2025",
       icon: "🤖",
       link: "https://drive.google.com/file/d/1KjDRQpykZaJnr9GTaPVMNIPD5bV4awCI/view?usp=sharing"
@@ -35,12 +39,14 @@ function Certifications() {
 
     {
       title: "Data Engineering And Big Data",
+      issuer: "HCL GUVI",
       year: "2026",
       icon: "📊",
       link: "https://www.guvi.in/share-certificate/p83808q97Tme71u7jS"
     },
     {
-      title: "Deloitte Data Analytics Simulation – Forage",
+      title: "Deloitte Data Analytics Simulation",
+      issuer: "Forage",
       year: "2025",
       icon: "📊",
       link: "https://drive.google.com/file/d/1CWSJa9jhajD36cMcC4SP9XpLNEmh6d1i/view?usp=sharing"
@@ -48,13 +54,15 @@ function Certifications() {
 
     {
       title: "AWS Solutions Architecture Job Simulation – Forage",
+       issuer: "Forage",
       year: "2025",
       icon: "🏗️",
       link: "https://drive.google.com/file/d/16-xExeyCFi41Pm2GkHTjY2NHAqH9fK2v/view?usp=sharing"
     },
 
     {
-      title: "Accenture Nordics Software Engineering Simulation – Forage",
+      title: "Accenture Nordics Software Engineering Simulation",
+      issuer: "Forage",
       year: "2025",
       icon: "💻",
       link: "https://drive.google.com/file/d/1kLTIGICKwk-WpiAyFzZNlkNl8bp6Sf25/view?usp=sharing"
@@ -65,8 +73,9 @@ function Certifications() {
   return (
     <section id="certifications" className="section">
 
-      <h2>Certifications</h2>
-
+      <h2>
+  Certifications (8)
+</h2>
       <div className="certifications-container">
 
         {certifications.map((cert, index) => (
@@ -83,6 +92,10 @@ function Certifications() {
             </div>
 
             <h3>{cert.title}</h3>
+
+<p className="issuer">
+  Certificate
+</p>
 
             <span className="cert-year">
               {cert.year}
